7a9821f9cdccce706c1649478681ecebc286ae12
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-09-07 18:19:06 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/AlexandreLuisSilva" target="_blank" rel="noreferrer">Alexandre Luis da Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/alexandre_luis_silva" target="_blank" rel="noreferrer">@alexandre_luis_silva</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
